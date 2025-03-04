3f1ebc56bbb0b4cbb7c962106243d05e7623dbc5
AdityaVKochar <80113212+AdityaVKochar@users.noreply.github.com>
2025-03-04 19:04:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const jwt = require('jsonwebtoken');
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cookieParser());    
+app.use(cookieParser()); 
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', function(req, res) {
     res.render('home');
